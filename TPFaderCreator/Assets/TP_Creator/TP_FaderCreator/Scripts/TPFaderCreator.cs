4e2fe45a49697d07526c0e634fad820a5bff2403
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2018-01-11 19:02:39 +0100

    Initialize project

---
+++ TPFaderCreator/Assets/TP_Creator/TP_FaderCreator/Scripts/TPFaderCreator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TP_Fader
+{
+    public class TPFaderCreator : MonoBehaviour
+    {
+        public Texture2D FadeTexture;
+
+        public void FadeTo(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
